afaa0101b421a9d248c7adc4ce39e56272d6eb6a
Funparks <funparksfun@gmail.com>
2026-04-06 03:25:22 +0200

    Add Hong Kong Disneyland, Universal Studios Singapore, Chimelong Ocean Kingdom - total 35 parks

---
+++ write_cok_parks.js
@@ -0,0 +1,13 @@
+﻿const fs=require('fs');
+function readJson(path){let raw=fs.readFileSync(path,'utf8');if(raw.charCodeAt(0)===0xFEFF)raw=raw.slice(1);return JSON.parse(raw);}
+
+const parkEntry={id:"chimelong_ocean_kingdom",thumbnailAsset:"assets/images/chimelong_ocean_kingdom/park_thumbnail.png",tailAsset:"",name:"Chimelong Ocean Kingdom",type:"Theme Park",entryPrice:{child:260,adult:380},currency:"CNY",openingHours:"10:00 - 20:00",lng:113.5620,country:"China",city:"Zhuhai, Guangdong",ticketsUrl:"https://www.chimelong.com/hengqin/ticket/",queueTimesId:0};
+
+const indexEntry={id:"chimelong_ocean_kingdom",name:"Chimelong Ocean Kingdom",city:"Zhuhai, Guangdong",country:"China",type:"Theme Park",lat:22.1100,lng:113.5620,thumbnail:"assets/images/chimelong_ocean_kingdom/park_thumbnail.png",website:"https://www.chimelong.com/hengqin/",ticketsUrl:"https://www.chimelong.com/hengqin/ticket/",detailAsset:"",openingHours:"10:00 - 20:00",entryPrices:{adult:380,child:260},currency:"CNY",queueTimesId:0};
+
+const parks=readJson('assets/data/parks.json');
+if(!parks.find(p=>p.id==='chimelong_ocean_kingdom')){parks.push(parkEntry);fs.writeFileSync('assets/data/parks.json',JSON.stringify(parks,null,4),'utf8');console.log('parks.json updated, total:',parks.length);}
+else{console.log('already exists');}
+const index=readJson('assets/data/parks/parks_index.json');
+if(!index.find(p=>p.id==='chimelong_ocean_kingdom')){index.push(indexEntry);fs.writeFileSync('assets/data/parks/parks_index.json',JSON.stringify(index,null,4),'utf8');console.log('parks_index.json updated, total:',index.length);}
+else{console.log('already exists');}
---
+++ write_hkd_i18n.js
@@ -0,0 +1,1 @@
+﻿const fs=require('fs');const d=JSON.parse(fs.readFileSync('hong_kong_disneyland_i18n_source.json','utf8'));fs.writeFileSync('assets/i18n/hong_kong_disneyland.json',JSON.stringify(d,null,2),'utf8');console.log('Done! Size:',fs.statSync('assets/i18n/hong_kong_disneyland.json').size,'bytes');
