3228b1a778909016ecdf663fbc72d6cfba5e1c05
Funparks <funparksfun@gmail.com>
2026-04-29 22:18:37 +0200

    Fix Firebase iOS crash - init in AppDelegate, disable proxy, build 112

---
+++ fix_firebase_delegate.js
@@ -0,0 +1,8 @@
+﻿const fs=require('fs');
+let c=fs.readFileSync('ios/Runner/Info.plist','utf8');
+if(!c.includes('FirebaseAppDelegateProxyEnabled')){
+  c=c.replace('</dict>\n</plist>',
+    '\t<key>FirebaseAppDelegateProxyEnabled</key>\n\t<false/>\n</dict>\n</plist>');
+  fs.writeFileSync('ios/Runner/Info.plist',c,'utf8');
+  console.log('Added FirebaseAppDelegateProxyEnabled=false');
+}
