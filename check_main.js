788b851ad330cd26854380e9cab9d95959467744
Funparks <funparksfun@gmail.com>
2026-05-01 08:36:30 +0200

    Disable Firebase auto-init on iOS, build 114

---
+++ check_main.js
@@ -0,0 +1,10 @@
+﻿const fs=require('fs');
+let c=fs.readFileSync('lib/main.dart','utf8');
+
+// Find and replace Firebase init
+const lines=c.split('\n');
+for(let i=0;i<lines.length;i++){
+  if(lines[i].includes('Firebase.initializeApp')||lines[i].includes('Firebase.apps.isEmpty')||lines[i].includes('firebase_options')){
+    console.log(i+1,lines[i].trim());
+  }
+}
---
+++ fix_firebase_autoinit.js
@@ -0,0 +1,11 @@
+﻿const fs=require('fs');
+let c=fs.readFileSync('ios/Runner/Info.plist','utf8');
+if(!c.includes('FIREBASE_AUTO_INIT_ENABLED')){
+  c=c.replace('</dict>\n</plist>',
+    '\t<key>FIREBASE_AUTO_INIT_ENABLED</key>\n\t<false/>\n</dict>\n</plist>');
+  fs.writeFileSync('ios/Runner/Info.plist',c,'utf8');
+  console.log('Disabled Firebase auto init');
+}
+// Also remove FirebaseAppDelegateProxyEnabled if it causes issues
+console.log('Info.plist tail:');
+console.log(fs.readFileSync('ios/Runner/Info.plist','utf8').split('\n').slice(-8).join('\n'));
