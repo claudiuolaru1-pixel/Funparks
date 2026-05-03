62dc80672d4872464a326bfe109c99e94aa81c47
Funparks <funparksfun@gmail.com>
2026-05-03 12:01:36 +0200

    Remove intl version constraint - let Flutter SDK manage it

---
+++ fix_intl3.js
@@ -0,0 +1,7 @@
+﻿const fs=require('fs');
+let c=fs.readFileSync('pubspec.yaml','utf8');
+// Remove intl line entirely - let Flutter SDK manage it
+c=c.replace(/\n  intl: \^0\.\d+\.\d+/,'');
+fs.writeFileSync('pubspec.yaml',c,'utf8');
+console.log('Removed intl constraint');
+console.log(fs.readFileSync('pubspec.yaml','utf8').split('\n').filter(l=>l.includes('intl')));
