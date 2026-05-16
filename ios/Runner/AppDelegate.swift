8356838947d8b3c561fcf8d04952d8b0c2eb02f7
Funparks <funparksfun@gmail.com>
2026-05-16 08:59:22 +0200

    Fix iOS auth: FirebaseApp.configure() in AppDelegate before plugins, build 193

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,14 +1,14 @@
-import Flutter
 import UIKit
-import GoogleMaps
+import Flutter
+import FirebaseCore
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIz.....................................cxI")
+    FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
