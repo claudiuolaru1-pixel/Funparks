f911d4aeb50747485967da1aaa4a197006f57802
Funparks <funparksfun@gmail.com>
2026-05-24 07:53:31 +0200

    Fix iOS Maps API key, build 222

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyCZoDy5KuQxIzNprpGrJT34KSias8GI-Ng")
+    GMSServices.provideAPIKey("AIzaSyDfu8zG-0zYT7puWDJIXkmCY_a0sC3TcxI")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
